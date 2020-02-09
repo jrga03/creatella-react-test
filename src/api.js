3843fd28d6275233028960cf0d3b8ad39b044d6b
Jason Acido <jasonacido@gmail.com>
2020-02-10 00:37:47 +0800

    Added function for fetching products

---
+++ src/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.API_URL;
+axios.defaults.headers.common[ 'X-Requested-With' ] = 'XMLHttpRequest';
+axios.defaults.headers.post[ 'Content-Type' ] = 'application/json';
+
+/**
+ * Fetches product list
+ * @param {object} params
+ * @param {number} params.page - Page number
+ * @param {number} [params.limit=10] - Limit of the number of items to be returned
+ * @param {string} [params.sort=''] - Sort results by one of ['price', 'size', 'id']
+ * @returns {object[]} List of products
+ */
+export async function getProducts({ page, limit = 15, sort = '' } = {}) {
+    const params = {
+        _page: page,
+        _limit: limit
+    };
+
+    if ( sort ) {
+        params._sort = sort;
+    }
+
+    const { data } = await axios({
+        url: '/api/products',
+        method: 'GET',
+        params
+    });
+
+    return data;
+}
