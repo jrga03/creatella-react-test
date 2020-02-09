53e29fe64f530ac6a069d7946fd7ea8fe84ed12b
Jason Acido <jasonacido@gmail.com>
2020-02-09 23:09:33 +0800

    Set up webpack and initial files for the App

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Wrapper } from './styles';
+
+function App() {
+    return (
+        <Wrapper>
+            Hello world!
+        </Wrapper>
+    );
+}
+
+export default App;
