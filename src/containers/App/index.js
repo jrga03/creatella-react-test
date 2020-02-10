468cf0f4d502ca4b91fa1276c0512efd07693f73
Jason Acido <jasonacido@gmail.com>
2020-02-10 23:51:42 +0800

    Add handling for sorting

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,34 +1,87 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import { getProducts } from '../../api';
 
-import { Wrapper } from './styles';
+import Loader from '../../components/Loader';
+import FaceList from '../../components/FaceList';
+import SortBy from '../../components/SortBy';
+
+import { Container, Header } from './styles';
+
+const SORT_OPTIONS = [
+    { value: 'price', label: 'Price' },
+    { value: 'size', label: 'Size' },
+    { value: 'id', label: 'ID' }
+];
 
 function App() {
+    const [ isLoading, setIsLoading ] = useState( false );
     const [ page, setPage ] = useState( 1 );
     const [ products, setProducts ] = useState([]);
+    const [ sortBy, setSortBy ] = useState( null );
+
+    const listRef = useRef( null );
 
     /**
-     * Called on initial loading of the page
-     * Fetches first set of data
+     * Create a scroll listener on component load
      */
     useEffect(() => {
-        async function getData() {
-            const products = await getProducts({ page });
-            setProducts( products );
-        }
+        console.log( 'onload' );
+        window.addEventListener( 'scroll', onScroll );
+        return () => window.removeEventListener( 'scroll', onScroll );
+    }, [])
 
+    useEffect(() => {
         getData();
-    }, []);
+    }, [sortBy]);
+
+    function onScroll( event ) {
+        console.log( 'onscroll', window.innerHeight, document.documentElement.scrollTop, document.documentElement.offsetHeight );
+        console.log( listRef.current.getBoundingClientRect() );
+        if ( window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight ) {
+            console.log('Fetch more list items!');
+        }
+    }
+
+    /**
+     * Fetch products
+     */
+    async function getData() {
+        setIsLoading( true );
+        const sort = sortBy ? sortBy.value : null;
+        const products = await getProducts({ page, limit: 50, sort });
+        setProducts( products );
+        setIsLoading( false );
+    }
+
+    /**
+     * Sets selected sort method to state
+     * @param {object} selected - Selected option
+     */
+    function onSortChange( selected ) {
+        const current = sortBy ? sortBy.value : null;
+        const next = selected ? selected.value : null;
+        if ( current !== next ) {
+            setProducts([]);
+            setPage( 1 );
+            setSortBy( selected );
+        }
+    }
 
     return (
-        <Wrapper>
-            { products.map(( product ) => (
-                <span key={ product.id }>
-                    { product.face }
-                </span>
-            )) }
-        </Wrapper>
+        <Container>
+            <Header>
+                <SortBy
+                    value={ sortBy }
+                    options={ SORT_OPTIONS }
+                    onChange={ onSortChange }
+                />
+            </Header>
+            { products.length === 0
+                ? <Loader />
+                : <FaceList faces={ products } onRef={ listRef } />
+            }
+        </Container>
     );
 }
 
