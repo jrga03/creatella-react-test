9e55807ec55fa72e31867fe5862efb0e89cc7e8d
Jason Acido <jasonacido@gmail.com>
2020-02-10 23:51:55 +0800

    Add styles for Header

--- src/containers/App/styles.js
+++ src/containers/App/styles.js
@@ -1,9 +1,25 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.main`
+export const Container = styled.main`
     height: 100vh;
     width: 100vw;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
+    padding-top: 58px;
+    padding-bottom: 8px;
+`;
+
+export const Header = styled.header`
+    width: 100%;
+    height: 50px;
+    background-color: #eaeaea;
+    position: fixed;
+    z-index: 10;
+    box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.2);
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 `;
