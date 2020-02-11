46c0529682b35ca13fceddd7a6d2fa90709146d8
Jason Acido <jasonacido@gmail.com>
2020-02-12 00:34:14 +0800

    Extract constants to a file

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+/**
+ * Constants
+ */
+export const LIMIT_PER_PAGE = 30;
+
+export const SORT_OPTIONS = [
+    { value: 'price', label: 'Price' },
+    { value: 'size', label: 'Size' },
+    { value: 'id', label: 'ID' }
+];
