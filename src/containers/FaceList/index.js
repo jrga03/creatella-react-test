97c59be3a1857263c9469c22873e5e47653f8c5c
Jason Acido <jasonacido@gmail.com>
2020-02-12 00:34:35 +0800

    Move FaceList to containers folder

--- src/containers/FaceList/index.js
+++ src/containers/FaceList/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Face from '../Face';
-import Loader from '../Loader';
+import Face from '../../components/Face';
+import Loader from '../../components/Loader';
 
 import ListWrapper from './styles';
 
