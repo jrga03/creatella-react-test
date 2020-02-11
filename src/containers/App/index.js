aa38b2e424116b0644a4497f5cff99b9a332a7de
Jason Acido <jasonacido@gmail.com>
2020-02-12 00:35:45 +0800

    Integrate with redux

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,123 +1,52 @@
-import React from 'react';
-
-import { getProducts } from '../../api';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 
 import { isBottomOfPage } from '../../utils/scroll';
 
+import { SORT_OPTIONS, LIMIT_PER_PAGE } from '../../constants';
+import { getProducts, loadMoreItems, setSortBy } from '../../actions';
+
 import Loader from '../../components/Loader';
-import FaceList from '../../components/FaceList';
 import SortBy from '../../components/SortBy';
 
-import { Container, Header } from './styles';
+import FaceList from '../FaceList';
 
-const SORT_OPTIONS = [
-    { value: 'price', label: 'Price' },
-    { value: 'size', label: 'Size' },
-    { value: 'id', label: 'ID' }
-];
+import { Container, Header, Footer } from './styles';
 
-class App extends React.Component {
-    constructor( props ) {
-        super( props );
+function App() {
+    const dispatch = useDispatch();
 
-        this.state = {
-            isLoading: false,
-            page: 1,
-            products: [],
-            preloadedProducts: [],
-            shouldRenderNextPage: true,
-            fetchingNextPage: false,
-            sortBy: null
+    const isLoading = useSelector(({ isLoading }) => isLoading );
+    const page = useSelector(({ page }) => page );
+    const products = useSelector(({ products }) => products );
+    const preloadedProducts = useSelector(({ preloadedProducts }) => preloadedProducts );
+    const isEndOfProductList = useSelector(({ isEndOfProductList }) => isEndOfProductList );
+    const fetchingNextPage = useSelector(({ fetchingNextPage }) => fetchingNextPage );
+    const sortBy = useSelector(({ sortBy }) => sortBy );
+
+    /**
+     * Create a scroll listener on component load
+     */
+    useEffect(() => {
+        const sort = sortBy ? sortBy.value : null;
+        dispatch( getProducts({ page, limit: LIMIT_PER_PAGE, sort }));
+
+        window.addEventListener( 'scroll', onScroll, { passive: true });
+        return () => window.removeEventListener( 'scroll', onScroll );
+    }, [])
+
+    useEffect(() => {
+        if ( preloadedProducts.length > 0 && !fetchingNextPage && isBottomOfPage()) {
+            dispatch( loadMoreItems());
         }
-    }
-
-    componentDidMount() {
-        window.addEventListener( 'scroll', this.onScroll );
-
-        this.getData();
-    }
-
-    componentDidUpdate( prevProps, prevState ) {
-        if ( prevState.sortBy !== this.state.sortBy ) {
-            this.getData();
-        }
-
-        if ( prevState.preloadedProducts.length !== this.state.preloadedProducts.length
-            && this.state.preloadedProducts.length > 0
-            && this.state.shouldRenderNextPage
-        ) {
-            this.loadMoreItems()
-        }
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener( 'scroll', this.onScroll );
-    }
+    }, [preloadedProducts, fetchingNextPage])
 
     /**
      * Callback for scroll event
      */
-    onScroll = () => {
-        /**
-         * Check if bottom of page is reached and
-         * page is not set to render the next page
-         */
-        if ( isBottomOfPage() && !this.state.shouldRenderNextPage ) {
-            this.setState({
-                isLoading: true,
-                shouldRenderNextPage: true
-            }, this.loadMoreItems );
-        }
-    }
-
-    /**
-     * Fetch products
-     */
-    getData = () => {
-        this.setState({ isLoading: true }, async () => {
-            const { page, sortBy } = this.state;
-            const sort = sortBy ? sortBy.value : null;
-            const products = await getProducts({ page, limit: 30, sort });
-
-            this.setState(( state ) => ({
-                products,
-                isLoading: false,
-                page: state.page + 1,
-                fetchingNextPage: true,
-                shouldRenderNextPage: false
-            }), this.preLoadNextPage );
-        });
-    }
-
-    /**
-     * Fetch next page in advance
-     */
-    preLoadNextPage = async () => {
-        const { page, sortBy } = this.state;
-        const sort = sortBy ? sortBy.value : null;
-        const products = await getProducts({ page, limit: 30, sort });
-
-        this.setState(( state ) => ({
-            preloadedProducts: products,
-            page: state.page + 1,
-            fetchingNextPage: false
-        }));
-    }
-
-    loadMoreItems = () => {
-        if ( this.state.preloadedProducts.length > 0 ) {
-            this.setState(( state ) => ({
-                products: [
-                    ...state.products,
-                    ...state.preloadedProducts
-                ],
-                preloadedProducts: [],
-                isLoading: false,
-                shouldRenderNextPage: false,
-                fetchingNextPage: true
-            }), this.preLoadNextPage );
-        } else if ( !this.state.fetchingNextPage ) {
-            this.setState({ fetchingNextPage: true }, this.preLoadNextPage );
+    function onScroll() {
+        if ( isBottomOfPage() && !isEndOfProductList ) {
+            dispatch( loadMoreItems());
         }
     }
 
@@ -125,40 +54,35 @@ class App extends React.Component {
      * Sets selected sort method to state
      * @param {object} selected - Selected option
      */
-    onSortChange = ( selected ) => {
-        const { sortBy } = this.state;
+    function onSortChange( selected ) {
         const current = sortBy ? sortBy.value : null;
         const next = selected ? selected.value : null;
         if ( current !== next ) {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-            this.setState({
-                products: [],
-                preloadedProducts: [],
-                page: 1,
-                sortBy: selected
-            });
+            dispatch( setSortBy( selected ));
+            dispatch( getProducts({ page: 1, limit: LIMIT_PER_PAGE, sort: next }));
         }
     }
 
-    render() {
-        const { sortBy, products, isLoading } = this.state;
-
-        return (
-            <Container>
-                <Header>
-                    <SortBy
-                        value={ sortBy }
-                        options={ SORT_OPTIONS }
-                        onChange={ this.onSortChange }
-                    />
-                </Header>
-                { products.length === 0
-                    ? <Loader />
-                    : <FaceList faces={ products } loading={ isLoading } />
-                }
-            </Container>
-        );
-    }
+    return (
+        <Container>
+            <Header>
+                <SortBy
+                    value={ sortBy }
+                    options={ SORT_OPTIONS }
+                    onChange={ onSortChange }
+                />
+            </Header>
+            { products.length === 0
+                ? <Loader />
+                : <FaceList faces={ products } loading={ isLoading } />
+            }
+            { isEndOfProductList && (
+                <Footer>
+                    ~ end of catalogue ~
+                </Footer>
+            ) }
+        </Container>
+    );
 }
 
 export default App;
