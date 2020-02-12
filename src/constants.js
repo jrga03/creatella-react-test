fd88f216b7a747ae26870439bbf8be3f170230a1
Jason Acido <jasonacido@gmail.com>
2020-02-12 11:52:18 +0800

    Add AD_FREQUENCY

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,8 @@
  */
 export const LIMIT_PER_PAGE = 30;
 
+export const AD_FREQUENCY = 20; // Every 20 items
+
 export const SORT_OPTIONS = [
     { value: 'price', label: 'Price' },
     { value: 'size', label: 'Size' },
